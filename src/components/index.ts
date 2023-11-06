ebcdd3d3658dadefd6043b3db8b70fdf7bca6a8e
Joma Work <johanmfc11@gmail.com>
2023-11-06 14:11:56 -0400

    Add new components and fonts, update styles and dependencies

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from './ClipboardIcon';
+export * from './FacebookIcon';
+export * from './InstagramIcon';
+export * from './TikTokIcon';
+export * from './WebsiteIcon';
+export * from './WhatsAppIcon';
